59c46017151f61ae4f8d703928f0c2dc507f215b
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-09 15:01:37 +0700

    add validate

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Post } from '@prisma/client';
 import { NewPost, UpdatePost } from 'src/graphql';
+import { PostNotFoundException } from '../exceptions/postNotFound.exception';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaError } from '../utils/prismaError';
 
 @Injectable()
 export class PostsService {
@@ -9,11 +12,16 @@ export class PostsService {
 
     // Get a single post
     async post(id: string): Promise<Post | null> {
-        return this.prisma.post.findUnique({
+        const post = await this.prisma.post.findUnique({
             where: {
                 id: parseInt(id),
             },
         });
+
+        if (!post) {
+            throw new PostNotFoundException(parseInt(id));
+        }
+        return post;
     }
 
     // Get multiple posts
@@ -31,23 +39,55 @@ export class PostsService {
     // Update a post
     async updatePost(params: UpdatePost): Promise<Post> {
         const { id, title, content } = params;
-        return this.prisma.post.update({
-            where: {
-                id: parseInt(id),
-            },
-            data: {
-                ...(title && { title }),
-                ...(content && { content }),
-            },
-        });
+        try {
+            const updatePost = await this.prisma.post.update({
+                where: {
+                    id: parseInt(id),
+                },
+                data: {
+                    ...(title && { title }),
+                    ...(content && { content }),
+                },
+            });
+            return updatePost;
+        } catch (error) {
+            if (
+                error instanceof PrismaClientKnownRequestError &&
+                error.code === PrismaError.RecordDoesNotExist
+            ) {
+                throw new PostNotFoundException(parseInt(id));
+            }
+            throw error;
+        }
     }
 
     // delete a post
     async deletePost(id: string): Promise<Post> {
-        return this.prisma.post.delete({
-            where: {
-                id: parseInt(id),
-            },
-        });
+        // const post = await this.prisma.post.findUnique({
+        //     where: {
+        //         id: parseInt(id),
+        //     },
+        // });
+
+        // if (!post) {
+        //     throw new PostNotFoundException(parseInt(id));
+        // }
+
+        try {
+            const deletePost = await this.prisma.post.delete({
+                where: {
+                    id: parseInt(id),
+                },
+            });
+            return deletePost;
+        } catch (error) {
+            if (
+                error instanceof PrismaClientKnownRequestError &&
+                error.code === PrismaError.RecordDoesNotExist
+            ) {
+                throw new PostNotFoundException(parseInt(id));
+            }
+            throw error;
+        }
     }
 }
